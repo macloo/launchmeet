a72aa619856b95fc8e16f653f61ea30edb4cc009
Mindy McAdams <mmcadams@gmail.com>
2015-01-30 16:50:24 -0500

    First commit

---
+++ scripts/project_data.js
@@ -0,0 +1,44 @@
+var projects = {
+  kill_facebook: {
+    title: "Kill Facebook",
+    brief: "Build a revolutionary social network that will kill Facebook dead.",
+    descrip: "I need a mobile developer who can create a video app that allows users to upload 30-second video diaries into a social network.",
+    created_by: 'sheryl'
+  },
+  money: {
+    title: "Dirty Money",
+    brief: "We're creating an infographic about money in politics.",
+    descrip: "",
+    created_by: 'sheryl'
+  },
+  danger: {
+    title: "Protect Journalists",
+    brief: "Keep reporters safe in dangerous locations.",
+    descrip: "",
+    created_by: 'sheryl'
+  },
+  mobile: {
+    title: "Better Mobile News",
+    brief: "We're changing the way mobile news is consumed.",
+    descrip: "",
+    created_by: 'sheryl'
+  }
+}
+
+var people = {
+  elvis: {
+    full_name: 'Elvis Presley',
+    profile_img: 'images/elvis.jpg',
+    tag: "Brand guru specializing in Silicon Valley makeovers.",
+    looking_for: "Paid work; fun, creative project",
+    skill_set: "Coding, Front-end developer, Marketing"
+  },
+  sheryl: {
+    full_name: "Sheryl Sandberg",
+    profile_img: "images/sheryl.jpg",
+    tag: "Budding entrepreneur with a nose for news.",
+    looking_for: "Designer, Mobile developer, Brand makeover specialist, Co-founder",
+    skill_set: "Editor, Writer, Entrepreneur",
+    project: 'kill_facebook'
+  }
+}
